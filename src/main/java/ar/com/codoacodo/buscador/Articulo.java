ef5a800490512a42c2a0b75edc304e946b12592e
Leii Dalle <leii16gd@gmail.com>
2022-11-08 18:11:37 -0300

    clase 2 java
    -create Buscador.java
    -create Articulo.java
    -create MainBuscador.java

---
+++ src/main/java/ar/com/codoacodo/buscador/Articulo.java
@@ -0,0 +1,20 @@
+package ar.com.codoacodo.buscador;
+
+public class Articulo {
+
+	//atributos
+	String img;
+	String titulo;
+	String autor;
+	Double precio;
+	
+	//constructor
+	Articulo(String img, String titulo, String autor, double precio){
+		
+	};
+
+	//metodos
+	public void detalle() {
+		System.out.println("detallando articulo..");
+	}
+}
